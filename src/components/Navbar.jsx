62476280ae3ce57e0c15e50e5373bdc417be8930
Yuusuf Abdullahi <yusufabdullahi2910@gmail.com>
2023-10-02 00:17:09 +0100

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,31 +15,36 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid ">
-          <h4 className="navbar-brand">Task Tracker</h4>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <h4 class="navbar-brand">Task Tracker</h4>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link to="/" class="nav-link active" aria-current="page">
                   Create Post
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link to="/read" className="nav-link">
+              <li class="nav-item">
+                <Link to="/read" class="nav-link active" aria-current="page">
                   All Post ({allusers.length})
                 </Link>
               </li>
             </ul>
+            <form class="d-flex" role="search">
             <input
-              className="form-control me-2 w-50"
+              class="form-control me-2 w-100"
               type="search"
               placeholder="Search"
               aria-label="Search"
               value={searchData}
               onChange={(e) => setSearchData(e.target.value)}
             />
+            </form>
           </div>
         </div>
       </nav>
